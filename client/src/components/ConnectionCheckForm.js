e5f6e2347bfc92a9bf1ba9d64f1b5f59e256d4a3
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-07-26 17:51:50 -0700

    Dynamic adding of URLs in UI

--- client/src/components/ConnectionCheckForm.js
+++ client/src/components/ConnectionCheckForm.js
@@ -1,25 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 
 const ConnectionCheckForm = (props) => {
+  const [urls, setUrls] = useState([""]);
+  const [results, setResults] = useState();
+
+  const addUrl = () => {
+    let newUrls = [...urls];
+    newUrls.push("");
+    setUrls(newUrls);
+  };
+
+  const changeHandler = (e, index) => {
+    let tempUrls = [...urls];
+    tempUrls[index] = e.target.value;
+    setUrls(tempUrls);
+  };
+
   return (
     <div className="card p-2 m-2 shadow">
       <h2 className="text-center">What connections would you like to check?</h2>
       <form>
         <div className="row justify-content-center my-2">
-          <button className="btn btn-primary" style={{width: "20rem"}}>Check Connections</button>
+          <button className="btn btn-primary" style={{ width: "20rem" }}>
+            Check Connections
+          </button>
         </div>
-        <div className="row align-items-center">
-          <div className="col-6">
-            <input className="form-control" type="text" placeholder="Enter URL" />
-          </div>
-          <div className="col-6">
-            <div className="alert alert-success py-2 mb-0">Online</div>
-            <div className="alert alert-danger py-2 mb-0">Offline</div>
-          </div>
+          {urls.map((url, index) => {
+            return (
+              <div className="row align-items-center my-2" key={index}>
+                <div className="col-6">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Enter URL"
+                    value={url}
+                    onChange={(e) => changeHandler(e, index)}
+                  />
+                </div>
+                <div className="col-6">
+                  {results ? (
+                    <div>
+                      <div className="alert alert-success py-2 mb-0">
+                        Online
+                      </div>
+                      <div className="alert alert-danger py-2 mb-0">
+                        Offline
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+            );
+          })}
+        <div className="btn btn-secondary my-2" onClick={addUrl}>
+          Add URL
         </div>
-        <div className="btn btn-secondary my-2">Add URL</div>
       </form>
     </div>
   );
